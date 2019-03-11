9274fc923a3253ae168ec8724d2f7967faf87d80
Walkinged <43610419+Walkinged@users.noreply.github.com>
2019-03-11 19:50:16 +0000

    Add files via upload
    Buttons to add and remove items to a till register

---
+++ Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace Buttons
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.coffeeBtn = new System.Windows.Forms.Button();
+            this.cakeBtn = new System.Windows.Forms.Button();
+            this.teaBtn = new System.Windows.Forms.Button();
+            this.salesLST = new System.Windows.Forms.ListBox();
+            this.priceLST = new System.Windows.Forms.ListBox();
+            this.totalTXT = new System.Windows.Forms.TextBox();
+            this.clearItemBtn = new System.Windows.Forms.Button();
+            this.totalLbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // coffeeBtn
+            // 
+            this.coffeeBtn.Location = new System.Drawing.Point(13, 28);
+            this.coffeeBtn.Name = "coffeeBtn";
+            this.coffeeBtn.Size = new System.Drawing.Size(75, 23);
+            this.coffeeBtn.TabIndex = 0;
+            this.coffeeBtn.Text = "Coffee";
+            this.coffeeBtn.UseVisualStyleBackColor = true;
+            this.coffeeBtn.Click += new System.EventHandler(this.coffeeBtn_Click);
+            // 
+            // cakeBtn
+            // 
+            this.cakeBtn.Location = new System.Drawing.Point(13, 68);
+            this.cakeBtn.Name = "cakeBtn";
+            this.cakeBtn.Size = new System.Drawing.Size(75, 23);
+            this.cakeBtn.TabIndex = 1;
+            this.cakeBtn.Text = "Cake";
+            this.cakeBtn.UseVisualStyleBackColor = true;
+            this.cakeBtn.Click += new System.EventHandler(this.cakeBtn_Click);
+            // 
+            // teaBtn
+            // 
+            this.teaBtn.Location = new System.Drawing.Point(13, 108);
+            this.teaBtn.Name = "teaBtn";
+            this.teaBtn.Size = new System.Drawing.Size(75, 23);
+            this.teaBtn.TabIndex = 2;
+            this.teaBtn.Text = "Tea";
+            this.teaBtn.UseVisualStyleBackColor = true;
+            this.teaBtn.Click += new System.EventHandler(this.teaBtn_Click);
+            // 
+            // salesLST
+            // 
+            this.salesLST.FormattingEnabled = true;
+            this.salesLST.Location = new System.Drawing.Point(145, 28);
+            this.salesLST.Name = "salesLST";
+            this.salesLST.Size = new System.Drawing.Size(171, 303);
+            this.salesLST.TabIndex = 3;
+            // 
+            // priceLST
+            // 
+            this.priceLST.FormattingEnabled = true;
+            this.priceLST.Location = new System.Drawing.Point(313, 28);
+            this.priceLST.Name = "priceLST";
+            this.priceLST.Size = new System.Drawing.Size(120, 303);
+            this.priceLST.TabIndex = 4;
+            // 
+            // totalTXT
+            // 
+            this.totalTXT.Location = new System.Drawing.Point(313, 337);
+            this.totalTXT.Name = "totalTXT";
+            this.totalTXT.Size = new System.Drawing.Size(120, 20);
+            this.totalTXT.TabIndex = 5;
+            // 
+            // clearItemBtn
+            // 
+            this.clearItemBtn.Location = new System.Drawing.Point(13, 148);
+            this.clearItemBtn.Name = "clearItemBtn";
+            this.clearItemBtn.Size = new System.Drawing.Size(75, 23);
+            this.clearItemBtn.TabIndex = 6;
+            this.clearItemBtn.Text = "Clear Item";
+            this.clearItemBtn.UseVisualStyleBackColor = true;
+            this.clearItemBtn.Click += new System.EventHandler(this.clearItemBtn_Click);
+            // 
+            // totalLbl
+            // 
+            this.totalLbl.AutoSize = true;
+            this.totalLbl.Location = new System.Drawing.Point(272, 343);
+            this.totalLbl.Name = "totalLbl";
+            this.totalLbl.Size = new System.Drawing.Size(37, 13);
+            this.totalLbl.TabIndex = 7;
+            this.totalLbl.Text = "Total: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(500, 379);
+            this.Controls.Add(this.totalLbl);
+            this.Controls.Add(this.clearItemBtn);
+            this.Controls.Add(this.totalTXT);
+            this.Controls.Add(this.priceLST);
+            this.Controls.Add(this.salesLST);
+            this.Controls.Add(this.teaBtn);
+            this.Controls.Add(this.cakeBtn);
+            this.Controls.Add(this.coffeeBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button coffeeBtn;
+        private System.Windows.Forms.Button cakeBtn;
+        private System.Windows.Forms.Button teaBtn;
+        private System.Windows.Forms.ListBox salesLST;
+        private System.Windows.Forms.ListBox priceLST;
+        private System.Windows.Forms.TextBox totalTXT;
+        private System.Windows.Forms.Button clearItemBtn;
+        private System.Windows.Forms.Label totalLbl;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Buttons
+{
+    
+
+    public partial class Form1 : Form
+    {
+        double Coffee = 1.25;                           // Public doubles to arrange prices for each item individually.
+        double Tea = 1.10;
+        double cake = 1.25;
+        double OrderTotal = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void coffeeBtn_Click(object sender, EventArgs e)
+        {
+            salesLST.Items.Add("Coffee ");
+            priceLST.Items.Add(String.Format("{0:0.00}", Coffee));
+            OrderTotal = OrderTotal + Coffee;
+            totalTXT.Text = OrderTotal.ToString();
+        }
+
+        private void cakeBtn_Click(object sender, EventArgs e)
+        {
+            salesLST.Items.Add("Cake ");
+            priceLST.Items.Add(String.Format("{0:0.00}", cake));
+            OrderTotal = OrderTotal + cake;
+            totalTXT.Text = OrderTotal.ToString();
+        }
+
+        private void teaBtn_Click(object sender, EventArgs e)
+        {
+            salesLST.Items.Add("Tea ");
+            priceLST.Items.Add(String.Format("{0:0.00}", Tea));
+            OrderTotal = OrderTotal + Tea;
+            totalTXT.Text = OrderTotal.ToString();
+        }
+
+        private void clearItemBtn_Click(object sender, EventArgs e)
+        {
+            if (this.priceLST.SelectedIndex >= 0)                                                  // Only run code if an item is selected.
+            {
+                double deductions = Convert.ToDouble(priceLST.GetItemText(priceLST.SelectedItem));   // Deducts selected item from the order list.
+
+                OrderTotal = OrderTotal - deductions;                                              // Order total deducts cost of removed item price.
+
+                totalTXT.Text = String.Format("{0:0.00}", OrderTotal).ToString();                  // Format then place the new orser total in the totalTXT box.
+
+                this.salesLST.Items.RemoveAt(this.priceLST.SelectedIndex);                         // Removes items from the sales list.
+                this.priceLST.Items.RemoveAt(this.priceLST.SelectedIndex);                         // Removes items from the price list.
+
+            }
+
+        }
+    }
+}
